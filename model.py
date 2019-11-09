eefc2af28b20dfdc4746ab60456b707ff4d578f9
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-11-09 11:09:39 +0000

    Initial commit

---
+++ model.py
@@ -0,0 +1,185 @@
+from __future__ import print_function
+import numpy as np
+import torch
+from torch import nn
+import sys
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+from utils import USE_CUDA
+from utils import get_torch_variable_from_np, get_data
+from utils import bilinear
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def log(*args, **kwargs):
+    print(*args,file=sys.stderr, **kwargs)
+
+
+
+class EN_Labeler(nn.Module):
+    def __init__(self, model_params):
+        super(EN_Labeler, self).__init__()
+        self.dropout = model_params['dropout']
+        self.dropout_word = model_params['dropout_word']
+        self.dropout_mlp = model_params['dropout_mlp']
+        self.batch_size = model_params['batch_size']
+
+        self.target_vocab_size = model_params['target_vocab_size']
+        self.use_flag_embedding = model_params['use_flag_embedding']
+        self.flag_emb_size = model_params['flag_embedding_size']
+        self.pretrain_emb_size = model_params['pretrain_emb_size']
+
+        self.bilstm_num_layers = model_params['bilstm_num_layers']
+        self.bilstm_hidden_size = model_params['bilstm_hidden_size']
+        self.use_biaffine = model_params['use_biaffine']
+
+        self.word_vocab_size = model_params['word_vocab_size']
+        self.fr_word_vocab_size = model_params['fr_word_vocab_size']
+        self.pretrain_vocab_size = model_params['pretrain_vocab_size']
+        self.fr_pretrain_vocab_size = model_params['fr_pretrain_vocab_size']
+        self.word_emb_size = model_params['word_emb_size']
+        self.pretrain_emb_size = model_params['pretrain_emb_size']
+        self.pretrain_emb_weight = model_params['pretrain_emb_weight']
+        self.fr_pretrain_emb_weight = model_params['fr_pretrain_emb_weight']
+
+        self.use_flag_embedding = model_params['use_flag_embedding']
+        self.flag_emb_size = model_params['flag_embedding_size']
+
+        self.pretrained_embedding = nn.Embedding(self.pretrain_vocab_size, self.pretrain_emb_size)
+        self.pretrained_embedding.weight.data.copy_(torch.from_numpy(self.pretrain_emb_weight))
+        self.fr_pretrained_embedding = nn.Embedding(self.fr_pretrain_vocab_size, self.pretrain_emb_size)
+        self.fr_pretrained_embedding.weight.data.copy_(torch.from_numpy(self.fr_pretrain_emb_weight))
+
+        self.id_embedding = nn.Embedding(100, self.flag_emb_size)
+        self.id_embedding.weight.data.uniform_(-1.0, 1.0)
+
+        if self.use_flag_embedding:
+            self.flag_embedding = nn.Embedding(2, self.flag_emb_size)
+            self.flag_embedding.weight.data.uniform_(-1.0, 1.0)
+
+        self.word_embedding = nn.Embedding(self.word_vocab_size, self.word_emb_size)
+        self.word_embedding.weight.data.uniform_(-1.0, 1.0)
+
+        self.fr_word_embedding = nn.Embedding(self.fr_word_vocab_size, self.word_emb_size)
+        self.fr_word_embedding.weight.data.uniform_(-1.0, 1.0)
+
+
+        self.word_dropout = nn.Dropout(p=0.5)
+        self.out_dropout = nn.Dropout(p=0.3)
+
+        input_emb_size = 0
+        if self.use_flag_embedding:
+            input_emb_size += self.flag_emb_size
+        else:
+            input_emb_size += 1
+
+
+        input_emb_size += self.pretrain_emb_size  # + self.pos_emb_size# + self.word_emb_size
+
+
+        if USE_CUDA:
+            self.bilstm_hidden_state = (
+            Variable(torch.randn(2 * self.bilstm_num_layers, self.batch_size, self.bilstm_hidden_size),
+                     requires_grad=True).cuda(),
+            Variable(torch.randn(2 * self.bilstm_num_layers, self.batch_size, self.bilstm_hidden_size),
+                     requires_grad=True).cuda())
+        else:
+            self.bilstm_hidden_state = (
+            Variable(torch.randn(2 * self.bilstm_num_layers, self.batch_size, self.bilstm_hidden_size),
+                     requires_grad=True),
+            Variable(torch.randn(2 * self.bilstm_num_layers, self.batch_size, self.bilstm_hidden_size),
+                     requires_grad=True))
+
+        self.bilstm_layer = nn.LSTM(input_size=300+self.flag_emb_size,
+                                    hidden_size=self.bilstm_hidden_size, num_layers=self.bilstm_num_layers,
+                                    bidirectional=True,
+                                    bias=True, batch_first=True)
+
+        if self.use_biaffine:
+            self.mlp_size = 300
+            self.rel_W = nn.Parameter(
+                torch.from_numpy(np.zeros((self.mlp_size + 1, self.target_vocab_size * (self.mlp_size + 1))).astype("float32")).to(
+                    device))
+            self.mlp_arg = nn.Sequential(nn.Linear(2 * self.bilstm_hidden_size, self.mlp_size), nn.ReLU())
+            self.mlp_pred = nn.Sequential(nn.Linear(2 * self.bilstm_hidden_size, self.mlp_size), nn.ReLU())
+
+
+
+        if USE_CUDA:
+            self.bilstm_hidden_state_word = (
+            Variable(torch.randn(2 * 2, self.batch_size, self.bilstm_hidden_size),
+                     requires_grad=True).cuda(),
+            Variable(torch.randn(2 * 2, self.batch_size, self.bilstm_hidden_size),
+                     requires_grad=True).cuda())
+        else:
+            self.bilstm_hidden_state_word = (
+            Variable(torch.randn(2 * 2, self.batch_size, self.bilstm_hidden_size),
+                     requires_grad=True),
+            Variable(torch.randn(2 * 2, self.batch_size, self.bilstm_hidden_size),
+                     requires_grad=True))
+
+        self.bilstm_layer_word = nn.LSTM(input_size=300+self.target_vocab_size+self.flag_emb_size,
+                                    hidden_size=self.bilstm_hidden_size, num_layers=2,
+                                    bidirectional=True,
+                                    bias=True, batch_first=True)
+        self.match_word = nn.Sequential(nn.Linear(2 * self.bilstm_hidden_size+300, self.mlp_size),
+                                        nn.ReLU(),
+                                        nn.Linear(self.mlp_size, self.target_vocab_size))
+
+
+
+    def forward(self, batch_input, lang='En'):
+        word_batch = get_torch_variable_from_np(batch_input['word'])
+        pretrain_batch = get_torch_variable_from_np(batch_input['pretrain'])
+        predicates_1D = batch_input['predicates_idx']
+        flag_batch = get_torch_variable_from_np(batch_input['flag'])
+        flag_emb = self.flag_embedding(flag_batch)
+
+        if lang == "En":
+            pretrain_emb = self.pretrained_embedding(pretrain_batch).detach()
+        else:
+            pretrain_emb = self.fr_pretrained_embedding(pretrain_batch).detach()
+
+        """
+        if lang == "En":
+            word_emb = self.word_embedding(word_batch)
+        else:
+            word_emb = self.fr_word_embedding(word_batch)
+        """
+
+
+        #input_emb = torch.cat((pretrain_emb, word_emb), 2)
+        input_emb = torch.cat((pretrain_emb, flag_emb), 2)
+        input_emb = self.word_dropout(input_emb)
+        seq_len = input_emb.shape[1]
+        bilstm_output, (_, bilstm_final_state) = self.bilstm_layer(input_emb, self.bilstm_hidden_state)
+        bilstm_output = bilstm_output.contiguous()
+        hidden_input = bilstm_output.view(bilstm_output.shape[0] * bilstm_output.shape[1], -1)
+        hidden_input = hidden_input.view(self.batch_size,seq_len, -1)
+        hidden_input = self.out_dropout(hidden_input)
+        arg_hidden = self.mlp_arg(hidden_input)
+        pred_recur = hidden_input[np.arange(0, self.batch_size), predicates_1D]
+        pred_hidden = self.mlp_pred(pred_recur)
+        SRL_output = bilinear(arg_hidden, self.rel_W, pred_hidden, self.mlp_size, seq_len, 1, self.batch_size,
+                          num_outputs=self.target_vocab_size, bias_x=True, bias_y=True)
+        SRL_output = SRL_output.view(self.batch_size * seq_len, -1)
+
+        SRL_input = SRL_output.view(self.batch_size, seq_len, -1)
+        compress_input = torch.cat((input_emb, SRL_input), 2)
+        bilstm_output_word, (_, bilstm_final_state_word) = self.bilstm_layer_word(compress_input, self.bilstm_hidden_state_word)
+        bilstm_output_word = bilstm_output_word.contiguous()
+        #hidden_input_word = bilstm_output_word.view(bilstm_output_word.shape[0] * bilstm_output_word.shape[1], -1)
+        pred_recur = bilstm_output_word[np.arange(0, self.batch_size), predicates_1D]
+        pred_recur = pred_recur.view(self.batch_size, self.bilstm_hidden_size*2)
+        pred_recur = pred_recur.unsqueeze(1).expand(self.batch_size, seq_len, self.bilstm_hidden_size*2)
+        combine = torch.cat((pred_recur, pretrain_emb), 2)
+        output_word = self.match_word(combine)
+        output_word = output_word.view(self.batch_size * seq_len, -1)
+        return SRL_output, output_word
+
+
+
+
+
+
